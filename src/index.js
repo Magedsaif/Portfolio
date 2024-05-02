2e0f49e1a90125ff7b1336015fdd7953ada94619
Magedsaf <Magedsaif21@gmail.com>
2024-05-02 14:25:04 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Analytics />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
